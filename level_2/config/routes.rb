76682ef322ad3d4f03d0ddeb272d16315bae47a7
baptiste dauphin <bap_dauphin@hotmail.fr>
2024-06-20 13:57:08 +0200

    generate routes and messages controller

--- level_2/config/routes.rb
+++ level_2/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :messages, only: [:index, :show, :create]
+  get "/tags/:name/messages", to: "messages#messages"
 end
