7243b4fe03142c4213ef211b0ca927ba85d12407
baptiste dauphin <bap_dauphin@hotmail.fr>
2024-06-20 14:12:46 +0200

    index and show actions and views for messages

--- level_2/app/controllers/messages_controller.rb
+++ level_2/app/controllers/messages_controller.rb
@@ -1,2 +1,28 @@
 class MessagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @message = Message.new
+    @messages = Message.order(created_at: :desc)
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    if @message.save!
+      redirect_to message_path(@message)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :tag_id)
+  end
 end
